8b75f3c521aad0f21fc09dca29607b520961b21a
myPyQuest <380663612689@yandex.ru>
2014-12-26 14:55:26 +0200

    fib

--- Numbers/fibonacci.py
+++ Numbers/fibonacci.py
@@ -1,1 +1,15 @@
-# def fib(n)
+def fib(n):
+
+    assert n > 0
+
+    series = [1]
+
+    while len(series) < n:
+        if len(series) == 1:
+            series.append(1)
+        else:
+            series.append(series[-1] + series[-2])
+
+
+    for i in range(len(series)):
+        pass
