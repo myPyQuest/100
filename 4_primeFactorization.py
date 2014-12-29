3c36487f755dcf993d6c432bf36df21c62bbc85f
4johndoe <bogdandemchenko@gmail.com>
2014-12-29 12:12:29 +0200

    refresh pycharm settings(tabs>spaces)

--- 4_primeFactorization.py
+++ 4_primeFactorization.py
@@ -1,7 +1,19 @@
-prime = []
+""" Prime Factorization
+Have the user enter a number and find all
+Prime Factors (if there are any) and display them."""
 
-for x in range(1,1000):
 
-	if :
-		pass
-	# prime.append(x)
+def is_prime(n):
+    if n % 2 == 0:
+        print(str(n) + ' % 2 False')
+        return False
+
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            print(str(n), ' % ', str(i), ' False')
+            return False
+    return True
+
+num = int(input('Lets try > '))
+
+print(is_prime(num))
