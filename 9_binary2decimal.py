4ca7da4a76d928e27dda779d14bc03f562ea17c2
4johndoe <bogdandemchenko@gmail.com>
2014-12-29 20:43:39 +0200

    bin2dec

--- 9_binary2decimal.py
+++ 9_binary2decimal.py
@@ -2,3 +2,24 @@
     Develop a converter to convert a decimal number to binary or a binary number to its decimal equivalent.
 """
 
+
+def bin2dec(num):
+    pass
+
+
+def dec2bin(num):
+    binary = ''
+
+    while num > 0:
+        binary += str(num % 2)
+        num //= 2
+
+    return binary[::-1]
+
+
+
+
+
+
+
+print(dec2bin(8))
