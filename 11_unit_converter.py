5de90a9ff49b7d6c90582dc407aa47fef219e57f
4johndoe <bogdandemchenko@gmail.com>
2014-12-30 12:54:02 +0200

    11 temp done

--- 11_unit_converter.py
+++ 11_unit_converter.py
@@ -3,6 +3,38 @@
     the type of unit they want to convert to and then the value. The program will then make the conversion.
 """
 
+
+def temp_cf(begin, t):
+    # convert C to F and back
+    if str(begin).lower() == 'c':
+        t_f = t * 9 / 5 + 32
+        return t_f
+    elif str(begin).lower() == 'f':
+        t_c = (t - 32) * 5 / 9
+        return t_c
+    else: print("Error input")
+
+
+def temp_ck(begin, t):
+    # convert C to K & back
+    if str(begin).lower() == 'c':
+        t_k = t + 273.15
+        return t_k
+    elif str(begin).lower() == 'k':
+        t_c = t - 273.15
+        return t_c
+    else: print("Error input")
+
+
+def temp_kf(begin, t):
+    # convert K to F & back
+    if str(begin).lower() == 'k':
+        t_f = (t - 273.15) * 9 / 5 + 32
+        return t_f
+    elif str(begin).lower() == 'f':
+        t_k = ((t - 32) * 5 / 9) + 273.15
+        return t_k
+
 print('''
 1. temp
 2. currency
@@ -21,20 +53,49 @@ if type_of_value == '1' or type_of_value == 'temp':
     t_in = input('from: ')
 
     if t_in == '1' or t_in.lower() == 'c':
-        t_out = input('to: ')
+        start = 'c'
+        t_out = input('to: ').lower()
 
-        if t_out == '1' or t_out.lower() == 'c':
+        if t_out == '1' or t_out == 'c':
             t_val = int(input("How much (C): "))
-            print(t_val * 1, 'C')
-        elif t_out == '2' or t_out.lower() == 'f':
+            print(t_val)
+        elif t_out == '2' or t_out == 'f':
             t_val = int(input("How much (C): "))
-            print(t_val * 9 / 5 + 32, 'F')
-        elif t_out == '3' or t_out.lower() == 'k':
+            print(temp_cf(start, t_val))
+        elif t_out == '3' or t_out == 'k':
             t_val = int(input("How much (C): "))
-            print(t_val + 273.15, 'K')
+            print(temp_ck(start, t_val))
+    elif t_in == '2' or t_in.lower() == 'f':
+        start = 'f'
+        t_out = input('to: ').lower()
 
-# elif type_of_value == '2' or type_of_value == 'currency':
-#     pass
+        if t_out == '1' or t_out == 'c':
+            t_val = int(input("How much (F): "))
+            print(temp_cf(start, t_val))
+        elif t_out == '2' or t_out == 'f':
+            t_val = int(input("How much (F): "))
+            print(t_val)
+        elif t_out == '3' or t_out == 'k':
+            t_val = int(input("How much (F): "))
+            print(temp_kf(start, t_val))
+    elif t_in == '3' or t_in.lower() == 'k':
+        start = 'k'
+        t_out = input('to: ').lower()
+
+        if t_out == '1' or t_out == 'c':
+            t_val = int(input("How much (K): "))
+            print(temp_ck(start, t_val))
+        elif t_out == '2' or t_out == 'f':
+            t_val = int(input("How much (K): "))
+            print(temp_kf(start, t_val))
+        elif t_out == '3' or t_out == 'k':
+            t_val = int(input("How much (K): "))
+            print(t_val)
+
+
+elif type_of_value == '2' or type_of_value == 'currency':
+    pass
+
 # elif type_of_value == '3' or type_of_value == 'volume':
 #     pass
 # elif type_of_value == '4' or type_of_value == 'mass':
