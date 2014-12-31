e9f50f901dbee7e48cca03f7a245bf65b44132de
myPyQuest <380663612689@yandex.ru>
2014-12-31 16:11:38 +0200

    text\countVow

---
+++ Text/countVowels.py
@@ -0,0 +1,16 @@
+s = input("give me a string of yours > ")
+
+# [ 'a', 'e', 'o', 'i', 'u']
+
+a = s.count('a')
+e = s.count('e')
+o = s.count('o')
+i = s.count('i')
+u = s.count('u')
+
+print("Here're " + str(a + e + o + i + u) + " vowels.")
+print("a is " + str(a))
+print("e is " + str(e))
+print("o is " + str(o))
+print("i is " + str(i))
+print("u is " + str(u))
