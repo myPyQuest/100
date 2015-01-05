f96bc885871317bd217004cde4f3e732d2795c2a
4johndoe <bogdandemchenko@gmail.com>
2015-01-05 17:23:40 +0200

    19 num names

--- 19_number_names.py
+++ 19_number_names.py
@@ -8,4 +8,116 @@
     (like zero, negative integers, and floating-point numbers).*
 """
 
-# later
+units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+places = ['', 'first', 'second', 'third', ]
+teenPlaces = ['tenth', 'eleventh', 'twelfth', 'thirteenth', 'fourteenth', 'fifteenth', 'sixteenth', 'seventeenth', 'eighteenth', 'nineteenth']
+tensPlaces = ['', 'tenth', 'twentieth', 'thirtieth', 'fortieth', 'fifthieth', 'sixthieth', 'seventieth', 'eightieth', 'ninetieth']
+
+
+def num_to_array(num):
+    num_array = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    num_array[0] = num/100000000 % 10  # Hundred Millions place
+    num_array[1] = num/10000000 % 10   # Ten Millions place
+    num_array[2] = num/1000000 % 10    # Millions place
+    num_array[3] = num/100000 % 10     # Hundred-thousands place
+    num_array[4] = num/10000 % 10      # Ten-thousands place
+    num_array[5] = num/1000 % 10       # Thousands place
+    num_array[6] = num/100 % 10        # Hundreds place
+    num_array[7] = num/10 % 10         # Tens place
+    num_array[8] = num/1 % 10          # Ones place
+    return num_array
+
+
+def has_trailing_zeros(num_array):
+    if num_array[6] != 0:
+        return num_array[7] == 0 and num_array[8] == 0
+    elif num_array[3] != 0 or num_array[4] != 0 or num_array[5] != 0:
+        return num_array[6] == 0 and num_array[7] == 0 and num_array[8] == 0
+    elif num_array[0] != 0 or num_array[1] != 0 or num_array[2] != 0:
+        return num_array[3] == 0 and num_array[4] == 0 and num_array[5] == 0 and num_array[6] == 0 and num_array[7] == 0\
+               and num_array[8] == 0
+
+
+def num_to_words(num):
+    num_array = num_to_array(num)
+    word = ''
+    if num == 0:
+        word = 'zero'
+    if 1 <= num <= 9:
+        word = units[num]
+    if 10 <= num <= 19:
+        word = teens[num % 10]
+    if 20 <= num <= 99:
+        word += tens[num/10]
+        if num_array[8] != 0:
+            word += ' ' + units[num % 10]
+
+    if 100 <= num <= 999:
+        word += units[num_array[6]] + ' hundred '
+        if not has_trailing_zeros(num_array):
+            word += num_to_words(num_array[7]*10+num_array[8])
+
+    if 1000 <= num <= 999999:
+        word += num_to_words(num_array[3]*100+num_array[4]*10+num_array[5]) + ' thousand '
+        if not has_trailing_zeros(num_array):
+            word += num_to_words(num_array[6]*100+num_array[7]*10+num_array[8])
+
+    if 1000000 <= num <= 999999999:
+        word += num_to_words(num_array[0]*100+num_array[1]*10+num_array[2]) + ' million '
+        if not has_trailing_zeros(num_array):
+            word += num_to_words(num_array[3]*100000+num_array[4]*10000+num_array[5]*1000+num_array[6]*100+num_array[7]*10+num_array[8])
+
+    word = ' '.join(word.split())
+    return word
+
+
+def num_to_place(num):
+    num_array = num_to_array(num)
+    place = ''
+    if (num == 0):
+        place = 'zeroth'
+    if 1 <= num <= 9:
+        place = places[num]
+    if 10 <= num <= 19:
+        place = teenPlaces[num % 10]
+    if 20 <= num <= 9:
+        if num_array[8] == 0:
+            place += tensPlaces[num/10]
+        else:
+            place += tens[num/10] + ' ' + places[num % 10]
+
+    if 100 <= num <= 999:
+        if has_trailing_zeros(num_array):
+            place += units[num_array[6]] + ' hundredth'
+        else:
+            place += units[num_array[6]] + ' hundred ' + num_to_place(num_array[7]*10+num_array[8])
+
+    if 1000 <= num <= 999999:
+        if has_trailing_zeros(num_array):
+            place += num_to_words(num_array[3]*100 + num_array[4]*10 + num_array[5]) + ' thousandth'
+        else:
+            place += num_to_words(num_array[3]*100+num_array[4]*10+num_array[5]) + ' thousand '
+            if not has_trailing_zeros(num_array):
+                place += num_to_place(num_array[6]*100+num_array[7]*10+num_array[8])
+
+    if 1000000 <= num <= 999999999:
+        if has_trailing_zeros(num_array):
+            place += num_to_words(num_array[0]*100 + num_array[1]*10 + num_array[2]) + ' millionth'
+        else:
+            place += num_to_words(num_array[0]*100+num_array[1]*10+num_array[2]) + ' million '
+            if not has_trailing_zeros(num_array):
+                place += num_to_place(num_array[3]*100000+num_array[4]*10000+num_array[5]*1000+num_array[6]*100+num_array[7]*10+num_array[8])
+    place = ' '.join(place.split())
+    return place
+
+
+def main(num):
+    number = input('Please enter a number: ')
+    print('Word:\t%s' % num_to_words(number))
+    print('Place:\t%s' % num_to_place(number))
+
+
+main()
--- 21_limit_calculator.py
+++ 21_limit_calculator.py
@@ -2,3 +2,103 @@
     Ask the user to enter f(x) and the limit value, then return the value of the limit statement
     *Optional: Make the calculator capable of supporting infinite limits.*
 """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from tkinter import *
+#
+#
+# class Calculator(Frame):
+#
+#     def frame(self, side):
+#         w = Frame(self)
+#         w.pack(side=side, expand=YES, fill=BOTH)
+#         return w
+#
+#     def button(self, root, side, text, command=None):
+#         w = Button(root, text=text, command=command)
+#         w.pack(side=side, expand=YES, fill=BOTH)
+#         return w
+#
+#     need_clr = False
+#
+#     def digit(self, digit):
+#         if self.need_clr:
+#             self.display.set('')
+#             self.need_clr = False
+#         self.display.set(self.display.get() + digit)
+#
+#     def sign(self):
+#         need_clr = False
+#         cont = self.display.get()
+#         if len(cont) > 0 and cont[0] == '-':
+#             self.display.set(cont[1:])
+#         else:
+#             self.display.set('-' + cont)
+#
+#     def oper(self, op):
+#         self.display.set(self.display.get() + ' ' + op + ' ')
+#         self.need_clr = False
+#
+#     def calc(self):
+#         try:
+#             self.display.set(eval(self.display.get()))
+#             self.need_clr = True
+#         except:
+#             showerror('Operation Error', 'Illegal Operation')
+#             self.display.set('')
+#             self.need_clr = False
+#
+#     def equals(self):
+#         try:
+#             result = eval(self.display.get())
+#             if result >= 1000:
+#                 result(calc)
+#         except:
+#             results("ERROR")
+#         display.delete(0, END)
+#         display.insert(0, display)
+#
+#     def __init__(self):
+#         Frame.__init__(self)
+#         self.option_add('*Font', 'Dotum 15')
+#         self.pack(expand=YES, fill=BOTH)
+#         self.master.title('Simple Calculator')
+#
+#         self.display = StringVar()
+#         e = Entry(self, relief=SUNKEN, textvariable=self.display)
+#         e.pack(side=TOP, expand=YES, fill=BOTH)
+#
+#         for key in ("123", "456", "789"):
+#             keyF = self.frame(TOP)
+#             for char in key:
+#                 self.button(keyF, LEFT, char,
+#                             lambda c=char: self.digit(c))
+#
+#         keyF = self.frame(TOP)
+#         self.button(keyF, LEFT, '0', lambda ch='0': self.digit(ch))
+#
+#         opsF = self.frame(TOP)
+#         for char in "+-=":
+#             if char == '=':
+#                 btn = self.button(opsF, LEFT, char, self.calc)
+#             else:
+#                 btn = self.button(opsF, LEFT, char,
+#                                   lambda w=self, s=char: w.oper(s))
+#
+#         clearF = self.frame(BOTTOM)
+#         self.button(clearF, LEFT, 'Clr', lambda w=self.display: w.set(''))
+#
+# if __name__ == '__main__':
+#     Calculator().mainloop()
