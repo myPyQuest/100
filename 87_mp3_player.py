d8db5b09cdbe0f921b7f9fc358b431f7f9e1bdc7
myPyQuest <380663612689@yandex.ru>
2015-01-11 19:25:43 +0200

    87 mp player

---
+++ 87_mp3_player.py
@@ -0,0 +1,17 @@
+""" 87**Mp3 Player**
+    A simple program for playing your favorite music files. Add features you though are missing from
+    your favorite music player.
+"""
+from tkinter import *
+import tkinter.filedialog as tk
+import tkinter.messagebox as tk2
+# import pygame
+
+
+# class Application(Frame):
+#
+#     def __init__(self, master):
+#         super(Application, self).__init__(master)
+#
+#         #self.create_widgets()
+#         self.playlistbox =
