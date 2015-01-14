183046eb30a84af83dbd2b8ca9a0aa951e1918a0
myPyQuest <380663612689@yandex.ru>
2015-01-14 17:39:17 +0200

    87_mp_player 2

--- 87_mp3_player.py
+++ 87_mp3_player.py
@@ -2,32 +2,12 @@
     A simple program for playing your favorite music files. Add features you though are missing from
     your favorite music player.
 """
-from mplayer import Player, CmdPrefix
+import mp3play
 
-# set default prefix for all Player instances
-Player.cmd_prefix = CmdPrefix.PAUSING_KEEP
+mp3 = mp3play.load(file)
+mp3.play()
 
-# since autospawn is True by default, no need to call player.spawn() manually
-player = Player()
-
-# play a file
-player.loadfile('file.mp3')
-
-# pause playback
-player.pause()
-
-# get title from metadata
-metadata = player.metadata or {}
-print(metadata.get('Title', ''))
-
-# print the filename
-print(player.filename)
-
-# seek +5 seconds
-player.time_pos += 5
-
-# set to fullscreen
-player.fullscreen = True
-
-# terminate MPlayer
-player.quit()
+# Let it play for up to 30 seconds, then stop it.
+import time
+# time.sleep(30, mp3.seconds())
+# mp3.stop()
